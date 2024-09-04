7f60a30086a599ccef1643a21f0153ebb38e0b22
rizqiyi <cold.gesture@gmail.com>
2024-09-04 13:57:03 +0700

    trigger push

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { UsersModule } from './users/users.module';
 import { PhotosModule } from './photos/photos.module';
 import { RolesModule } from './roles/roles.module';
 import { DrainageModule } from './drainage/drainage.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UsersModule, PhotosModule, RolesModule, DrainageModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
